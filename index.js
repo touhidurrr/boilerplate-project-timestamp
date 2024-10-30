272a859ef99e00c111242e2faef6ccaac3fbd8ad
Md. Touhidur Rahman <touhidurrahmant1@gmail.com>
2024-10-30 22:52:47 +0000

    fix more issues

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,17 +21,23 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 // timestamp api
 app.get("/api/:date?", function (req, res) {
-  const millis = +req.params.date;
+  let date;
 
-  const date = new Date(
-    !isNaN(millis) ? millis :
-      req.params.date
-  );
+  if (typeof (req.params.date) !== "string") {
+    date = new Date();
+  } else {
+    const millis = +req.params.date;
+    
+    date = new Date(
+      !isNaN(millis) ? millis :
+        req.params.date
+    );
+  }
 
   if (date === "Invalid Date") {
     res.json({ error: "Invalid Date" });
